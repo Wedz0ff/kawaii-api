89f57f106bb69d08bd034fbe7a43f802926c75bf
Wedz0ff <me@wed.tf>
2025-04-13 10:07:23 -0300

    feat: oops, forgot cors xD

--- src/main.py
+++ src/main.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.inmemory import InMemoryBackend
-from fastapi_cache.decorator import cache
+from fastapi.middleware.cors import CORSMiddleware
 from contextlib import asynccontextmanager
 from src.app.routes import api_router
 
@@ -13,6 +13,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(lifespan=lifespan)
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 app.include_router(api_router)
